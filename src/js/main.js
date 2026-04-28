575f794314b30575d0efbcca19a3fc7e122fe2e0
austinsego12 <dragoastee123@gmail.com>
2026-04-28 11:59:32 -0500

    finished the nps assignment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimerLink = document.querySelector(".disclaimer > a");
+disclaimerLink.href = parkData.url;
+disclaimerLink.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const heroImage = document.querySelector(".hero-banner__image");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
+function parkInfoTemplate(info) {
+  return `
+    <a href="${info.url}" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>
+  `;
+}
+
+const heroInfo = document.querySelector(".hero-banner__info");
+heroInfo.innerHTML = parkInfoTemplate(parkData);
