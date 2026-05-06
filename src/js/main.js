424027e22707a9ca25a115ded83fbbcd9d9a35e2
austinsego12 <dragoastee123@gmail.com>
2026-05-05 23:05:44 -0500

    removed parkinfolinks from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -38,28 +38,6 @@ function setParkIntro(data) {
   `;
 }
 
-const parkInfoLinks = [
-  {
-    name: "Current Conditions &#x203A;",
-    link: "conditions.html",
-    image: parkData.images[2].url,
-    description:
-      "See what conditions to expect in the park before leaving on your trip!"
-  },
-  {
-    name: "Fees and Passes &#x203A;",
-    link: "fees.html",
-    image: parkData.images[3].url,
-    description: "Learn about the fees and passes that are available."
-  },
-  {
-    name: "Visitor Centers &#x203A;",
-    link: "visitor_centers.html",
-    image: parkData.images[9].url,
-    description: "Learn about the visitor centers in the park."
-  }
-];
-
 function mediaCardTemplate(info) {
   return `
     <div class="media-card">
