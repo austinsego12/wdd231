eac4fbb49e689ffff7f5681ed4130f0888de2e2d
austinsego12 <dragoastee123@gmail.com>
2026-05-05 23:11:55 -0500

    potentially fixed it (finally :)

--- src/js/main.js
+++ src/js/main.js
@@ -38,18 +38,6 @@ function setParkIntro(data) {
   `;
 }
 
-function mediaCardTemplate(info) {
-  return `
-    <div class="media-card">
-      <a href="${info.link}">
-        <img src="${info.image}" alt="${info.name}" class="media-card__img">
-        <h3 class="media-card__title">${info.name}</h3>
-      </a>
-      <p>${info.description}</p>
-    </div>
-  `;
-}
-
 function setParkInfoLinks(data) {
   const info = document.querySelector(".info");
   const html = data.map(mediaCardTemplate);
